acb00e671e94e03c565ef24040b199fd97cc315d
cemarichal <marichal@kth.se>
2021-02-09 14:27:43 +0100

    Creating structure

---
+++ app/src/main/java/com/kth/id2216/group3/time/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.kth.id2216.group3.time
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
---
+++ app/src/main/java/com/kth/id2216/group3/time/TimerActivity.kt
@@ -0,0 +1,11 @@
+package com.kth.id2216.group3.time
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
