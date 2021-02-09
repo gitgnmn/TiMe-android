e58d6a3e95882dd0678c1f9209136d049d9d341c
cemarichal <marichal@kth.se>
2021-02-09 14:56:14 +0100

    Data structure

---
+++ app/src/main/java/com/kth/id2216/group3/time/bo/Session.kt
@@ -0,0 +1,10 @@
+package com.kth.id2216.group3.time.bo
+
+class Session(val id:Int) {
+
+    var duration = 0
+
+    init {
+        //define the id
+    }
+}
---
+++ app/src/main/java/com/kth/id2216/group3/time/bo/TimerState.kt
@@ -0,0 +1,7 @@
+package com.kth.id2216.group3.time.bo
+
+enum class TimerState {
+    STOPPED,
+    PAUSED,
+    RUNNING
+}
