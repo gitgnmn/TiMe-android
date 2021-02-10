94e0276a7a8866e688aaedd0213dfc6ad46ff3a2
Amanda <anee@kth.se>
2021-02-10 14:52:32 +0100

    Working staggered grid view and .idea the .idea is to prevent further weird configuration errors

--- app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
+++ app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
@@ -2,16 +2,17 @@ package com.kth.id2216.group3.time
 
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -19,6 +20,9 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var recyclerView: RecyclerView
+
+    private var images = ArrayList(listOf(R.drawable.img_1, R.drawable.img_2))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +44,19 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_new_category, R.id.nav_settings), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Getting reference of recyclerView
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+
+        // Setting the layout as Staggered Grid for vertical orientation
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        recyclerView.layoutManager = staggeredGridLayoutManager
+
+        // Sending reference and data to Adapter
+        val adapter = Adapter(this@MainActivity, images)
+
+        // Setting Adapter to RecyclerView
+        recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
