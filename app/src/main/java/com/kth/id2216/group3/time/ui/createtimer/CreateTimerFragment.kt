392087b9447dfc27c231b84a437959618fdec272
cemarichal <marichal@kth.se>
2021-02-16 10:45:31 +0100

    Deleting unnecessary parts

--- app/src/main/java/com/kth/id2216/group3/time/ui/createtimer/CreateTimerFragment.kt
+++ app/src/main/java/com/kth/id2216/group3/time/ui/createtimer/CreateTimerFragment.kt
@@ -10,12 +10,6 @@ import com.kth.id2216.group3.time.R
 import com.kth.id2216.group3.time.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass handling timer creation
  */
@@ -23,18 +17,6 @@ private const val ARG_PARAM2 = "param2"
 class CreateTimerFragment : Fragment() {
 
     private val createTimerViewModel: CreateTimerViewModel by viewModels()
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
