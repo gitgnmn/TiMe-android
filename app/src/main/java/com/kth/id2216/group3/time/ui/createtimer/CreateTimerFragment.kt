90f906d96b7afdc929b7351e8bb7bc237f49eaa4
cemarichal <marichal@kth.se>
2021-02-16 11:18:51 +0100

    Creation connected to db and user data handled

--- app/src/main/java/com/kth/id2216/group3/time/ui/createtimer/CreateTimerFragment.kt
+++ app/src/main/java/com/kth/id2216/group3/time/ui/createtimer/CreateTimerFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.kth.id2216.group3.time.R
 import com.kth.id2216.group3.time.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +29,31 @@ class CreateTimerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_timer, container, false)
+
+
+        val root = inflater.inflate(R.layout.fragment_create_timer, container, false)
+
+        val createButton: Button = root.findViewById(R.id.create_timer_button)
+        createButton.setOnClickListener {
+            val nameEditText: EditText = root.findViewById(R.id.create_timer_name)
+            val goalEditText: EditText = root.findViewById(R.id.create_timer_goal)
+            val goalString = goalEditText.text.toString()
+
+            // check if te value of the goal is set up and positive
+            if (goalString != "" && Integer.parseInt(goalString) > 0) {
+                createTimerViewModel.addTimer(
+                        nameEditText.text.toString(),
+                        Integer.parseInt(goalEditText.text.toString())
+                )
+                root.findNavController().navigate(R.id.nav_home)
+            } else {
+                val toast = Toast.makeText(this.context, getString(R.string.message_positive_goal), Toast.LENGTH_LONG)
+                toast.show()
+            }
+        }
+
+
+        return root
     }
 
 
