2c687f1e498f3281b87085462d8e9fb74ed660df
tjohann <tjohann@kth.se>
2021-02-18 19:09:14 +0100

    TimerView

--- app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
+++ app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kth.id2216.group3.time
 
 import android.content.Intent
+import android.widget.Button
+import android.widget.ImageButton
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -24,16 +26,13 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     //private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -46,10 +45,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { navController.navigate(R.id.createTimer) }
-
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id in arrayOf(R.id.createTimer, R.id.nav_categories))
                 fab.hide()
@@ -57,21 +54,33 @@ class MainActivity : AppCompatActivity() {
                 fab.show()
         }
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
     fun launchTimerActivity(view: View?) {
         Log.d("Switching Activity", "TimerActivity")
         val intent = Intent(this@MainActivity, TimerActivity::class.java)
         startActivity(intent)
+    }
+
+    fun toggleTimer(view: View?){
+        var timerIsRunning = 1  // Stuck on first listItem Need to be a global value
+        val btnStart = findViewById(R.id.item_button_start) as Button // Stuck on first listItem
+        val btnPause = findViewById(R.id.item_button_stop) as Button // Stuck on first listItem
+
+        if(timerIsRunning == 1) {
+            btnPause.setVisibility(View.VISIBLE);
+            btnStart.setVisibility(View.INVISIBLE);
+        }
+        else if(timerIsRunning == 0){
+            btnPause.setVisibility(View.INVISIBLE);
+            btnStart.setVisibility(View.VISIBLE);
+        }
     }
 }
