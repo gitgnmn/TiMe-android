eedee73a6541ad3fbcfc031ddc47e01ab14e0321
cemarichal <marichal@kth.se>
2021-02-19 15:15:38 +0100

    Adding category list display

--- app/src/main/java/com/kth/id2216/group3/time/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/kth/id2216/group3/time/ui/categories/CategoriesViewModel.kt
@@ -3,9 +3,11 @@ package com.kth.id2216.group3.time.ui.categories
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.kth.id2216.group3.time.data.entities.Category
 import com.kth.id2216.group3.time.data.repositories.CategoryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -13,8 +15,21 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class CategoriesViewModel @Inject constructor(
-    categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository
 ): ViewModel() {
 
     val categories: LiveData<List<Category>> = categoryRepository.getAll().asLiveData()
+
+    fun addCategory(category: Category) {
+        viewModelScope.launch {
+            categoryRepository.insert(category)
+        }
+    }
+
+    fun getAllCategories(): LiveData<List<Category>> {
+        viewModelScope.launch {
+            val categories = categoryRepository.getAll().asLiveData()
+        }
+        return categories
+    }
 }
