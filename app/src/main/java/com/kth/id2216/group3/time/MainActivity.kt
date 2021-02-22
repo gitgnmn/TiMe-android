0816f6b92a53c1ab1b138984e671c0efffdcfe83
Tobias Johannesson <48130345+Surejohan@users.noreply.github.com>
2021-02-22 10:16:48 +0100

    TimerActivity (#23)
    * TimerView
    
    * TimerActivity now has correct data

--- app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
+++ app/src/main/java/com/kth/id2216/group3/time/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.kth.id2216.group3.time
 
 import android.content.Intent
-import android.widget.Button
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -37,9 +39,9 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_settings
-            ), drawerLayout
+                setOf(
+                        R.id.nav_home, R.id.nav_settings
+                ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
@@ -57,6 +59,29 @@ class MainActivity : AppCompatActivity() {
     fun launchTimerActivity(view: View?) {
         Log.d("Switching Activity", "TimerActivity")
         val intent = Intent(this@MainActivity, TimerActivity::class.java)
+
+        // Send the data to timerActivity
+        val hourView:TextView = (view as ViewGroup).getChildAt(1) as TextView
+        val hours = hourView.text.toString()
+        println("This should be the hours: $hours")
+        intent.putExtra("hours", hours)
+
+        val goalView:TextView = (view as ViewGroup).getChildAt(2) as TextView
+        val goal = goalView.text.toString()
+        println("This should be the goal: $hours")
+        intent.putExtra("goal", goal)
+
+        val nameView:TextView = (view as ViewGroup).getChildAt(3) as TextView
+        val name = nameView.text.toString()
+        println("This should be the name: $name")
+        intent.putExtra("name", name)
+
+        val categoryView:TextView = (view as ViewGroup).getChildAt(4) as TextView
+        val category = categoryView.text.toString()
+        println("This should be the category: $category")
+        intent.putExtra("category", category)
+        // End of putting extra data into intent
+
         startActivity(intent)
     }
 
