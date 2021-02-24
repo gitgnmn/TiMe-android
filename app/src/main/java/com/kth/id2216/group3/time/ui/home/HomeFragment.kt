a0e59da5029e36ee2981ecc7a2175d87a861a0f9
Amanda <anee@kth.se>
2021-02-24 18:27:32 +0100

    Removed test data and created keystore

--- app/src/main/java/com/kth/id2216/group3/time/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kth/id2216/group3/time/ui/home/HomeFragment.kt
@@ -31,9 +31,9 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Insert example data
-        val timer1 = Timer(id = 1, name = "Best Course Ever", goal = Duration.ofHours(14))
-        val timer2 = Timer(id = 2, name = "Example Course", goal = Duration.ofHours(20), time = Duration.ofHours(10))
-        val timer3 = Timer(id = 3, name = "Building some awesome robots", goal = Duration.ofHours(20), time = Duration.ofHours(1))
+        //val timer1 = Timer(id = 1, name = "Best Course Ever", goal = Duration.ofHours(14))
+        //val timer2 = Timer(id = 2, name = "Example Course", goal = Duration.ofHours(20), time = Duration.ofHours(10))
+        //val timer3 = Timer(id = 3, name = "Building some awesome robots", goal = Duration.ofHours(20), time = Duration.ofHours(1))
 
         val adapter = TimerAdapter(context)
         //get data
@@ -42,9 +42,9 @@ class HomeFragment : Fragment() {
         })
 
         // Add example data to db
-        homeViewModel.addTimer(timer1)
-        homeViewModel.addTimer(timer2)
-        homeViewModel.addTimer(timer3)
+        //homeViewModel.addTimer(timer1)
+        //homeViewModel.addTimer(timer2)
+        //homeViewModel.addTimer(timer3)
 
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val recyclerView: RecyclerView  = root.findViewById(R.id.timerRecyclerView)
